cff3d7a2359cb361cfaaf24acda7a8daf4013bce
Mubiitoedward <mubiitoedward6@gmail.com>
2024-09-11 22:37:14 +0300

    email

--- contactform.php
+++ contactform.php
@@ -3,7 +3,7 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $subject =  $_POST['subject'];
 $message = $_POST['message'];
-$to = $_POST['mubiitoedward@gmail.com'];
+$to = $_POST['mubiitoedward6@gmail.com'];
 
 $headers = "From :".$name."\r\n" .
 "CC: support@bmaxigraphics.com";
